f60dd2439bf05e7e125fe4d4883225385158730d
inoveAlumnos <alumnos@inove.com.ar>
2022-03-08 09:25:08 -0300

    Refactor de la estructura del repo

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Sobre comentarios:
+
+// Este es un comentario de una sola línea (se usan dos / al inicio)
+
+/* Este es un comentario de más de una línea de código,
+se utiliza para apertura y cierre estos simbolos
+*/
+
+console.log("Hola Mundo JS desde app.js");
+
